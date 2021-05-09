ef58876311a47607f3a9d13be7efe4c8239a4cb5
Artemis_Toh <tohwenxiang@gmail.com>
2021-05-09 23:21:02 +0800

    Keep track of whether a cell have been visited and which walls it has

--- cell.js
+++ cell.js
@@ -3,5 +3,13 @@ class Cell {
     this.P5 = P5;
     this.column = column;
     this.row = row;
+    
+    this.isVisited = false;
+    this.walls = {
+      top: false, 
+      right: false,
+      bottom: false, 
+      left: false
+    };
   }
 }
