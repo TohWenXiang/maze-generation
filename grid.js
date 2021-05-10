a4c9b003458f3ec243b2040e5ae9fdacc54af03d
Wen Xiang <tohwenxiang@gmail.com>
2021-05-10 15:23:35 +0800

    Add a method to convert cell index to pixel position

--- grid.js
+++ grid.js
@@ -1,58 +1,59 @@
 class Grid {
-  constructor(P5, columns, rows, cellSize) {
-    this.P5 = P5;
-    
-    this.columns = columns;
-    this.rows = rows;
-    this.cellSize = cellSize;
-    
-    this.cells = new Array(this.columns * this.rows);
-    
-    this.fillCells();
-  }
-  
-  fillCells() {
-    for(let r = 0; r < this.rows; r++) {
-      for(let c = 0; c < this.columns; c++) {
-        this.cells[r * this.columns + c] = new Cell(this.P5, c, r);
-      }
+    constructor(P5, columns, rows, cellSize) {
+        this.P5 = P5;
+
+        this.columns = columns;
+        this.rows = rows;
+        this.cellSize = cellSize;
+
+        this.cells = new Array(this.columns * this.rows);
+
+        this.fillCells();
     }
-  }
-  
-  isCellIndexOutOfBounds(column, row) {
-    if(
-      column < 0 || 
-      column >= this.columns || 
-      row < 0 || 
-      row >= this.rows
-      ) {
-      return true;
-    }
-    return false;
-  }
-  
-  getCell(column, row) {
-    if(this.isCellIndexOutOfBounds(column, row)) {
-      return undefined;
-    }
-    return this.cells[row * this.columns + column];
-  }
-  
-  getNeighbouringCells(column, row) {
-    let neighbouringCells = [];
-    
-    for(let r = row - 1; r <= row + 1; r++) {
-      for(let c = column - 1; c <= column + 1; c++) {
-        if(
-          c === column && 
-          r === row
-          ) {
-          continue;
+
+    fillCells() {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.columns; c++) {
+                this.cells[r * this.columns + c] = new Cell(this.P5, c, r);
+            }
         }
-        
-        neighbouringCells.push(this.getCell(c, r));
-      }
     }
-    return neighbouringCells;
-  }
+
+    isCellIndexOutOfBounds(column, row) {
+        if (
+            column < 0 ||
+            column >= this.columns ||
+            row < 0 ||
+            row >= this.rows
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    getCell(column, row) {
+        if (this.isCellIndexOutOfBounds(column, row)) {
+            return undefined;
+        }
+        return this.cells[row * this.columns + column];
+    }
+
+    getNeighbouringCells(column, row) {
+        let neighbouringCells = [];
+
+        for (let r = row - 1; r <= row + 1; r++) {
+            for (let c = column - 1; c <= column + 1; c++) {
+                if (c === column && r === row) {
+                    continue;
+                }
+
+                neighbouringCells.push(this.getCell(c, r));
+            }
+        }
+        return neighbouringCells;
+    }
+
+    toPixelPosition(cell) {
+        return [cell.column * this.cellSize, cell.row * this.cellSize];
+    }
 }
