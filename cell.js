71d5e8ef495b2cd9eb86757c585d515fe2396b6e
Wen Xiang <tohwenxiang@gmail.com>
2021-05-10 14:39:27 +0800

    Add drawing of walls for each cells

--- cell.js
+++ cell.js
@@ -1,15 +1,13 @@
 class Cell {
-  constructor(P5, column, row) {
-    this.P5 = P5;
-    this.column = column;
-    this.row = row;
-    
-    this.isVisited = false;
-    this.walls = {
-      top: false, 
-      right: false,
-      bottom: false, 
-      left: false
-    };
-  }
+    constructor(P5, column, row) {
+        this.P5 = P5;
+        this.column = column;
+        this.row = row;
+
+        this.isVisited = false;
+        this.walls = {
+            right: true,
+            bottom: true,
+        };
+    }
 }
--- maze-renderer.js
+++ maze-renderer.js
@@ -1,10 +1,28 @@
 class MazeRenderer {
-  constructor(P5, maze) {
-    this.P5 = P5;
-    this.maze = maze;
-  }
-  
-  draw() {
-    
-  }
+    constructor(P5, maze) {
+        this.P5 = P5;
+        this.grid = maze.grid;
+    }
+
+    draw() {
+        this.P5.stroke(255);
+        this.drawWalls();
+    }
+
+    drawWalls() {
+        this.grid.cells.forEach((cell) => {
+            let x1 = cell.column * this.grid.cellSize;
+            let x2 = x1 + this.grid.cellSize;
+            let y1 = cell.row * this.grid.cellSize;
+            let y2 = y1 + this.grid.cellSize;
+
+            if (cell.walls.right) {
+                this.P5.line(x2, y1, x2, y2);
+            }
+
+            if (cell.walls.bottom) {
+                this.P5.line(x1, y2, x2, y2);
+            }
+        });
+    }
 }
