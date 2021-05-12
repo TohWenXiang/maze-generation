3f59d7d53791a40bb9b00c7f90ea2dd7e46b4948
Wen Xiang <tohwenxiang@gmail.com>
2021-05-12 14:00:14 +0800

    Color maze agent current cell

--- maze-renderer.js
+++ maze-renderer.js
@@ -2,9 +2,11 @@ class MazeRenderer {
     constructor(P5, maze) {
         this.P5 = P5;
         this.grid = maze.grid;
+        this.agent = maze.mazeAgent;
 
         this.unVisitedCellColor = this.P5.color(10, 25, 75);
         this.visitedCellColor = this.P5.color(10, 75, 25);
+        this.agentCurrentLocationColor = this.P5.color(75, 25, 10);
     }
 
     draw() {
@@ -21,6 +23,12 @@ class MazeRenderer {
             this.P5.fill(this.visitedCellColor);
         } else {
             this.P5.fill(this.unVisitedCellColor);
+        }
+        if (
+            this.agent.current.c === cell.column &&
+            this.agent.current.r === cell.row
+        ) {
+            this.P5.fill(this.agentCurrentLocationColor);
         }
         this.P5.square(cell.pixelPos.x, cell.pixelPos.y, cell.cellSize);
         this.P5.pop();
