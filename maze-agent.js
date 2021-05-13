a33047bb2671399a9a6105662d19e6b0695c3050
Wen Xiang <tohwenxiang@gmail.com>
2021-05-13 13:20:34 +0800

    Choose the initial cell, mark it as visited and push it to the stack

--- maze-agent.js
+++ maze-agent.js
@@ -1,51 +1,54 @@
 class MazeAgent {
-  constructor(column, row, grid) {
-    this.start = {
-      c: column, 
-      r: row
+    constructor(column, row, grid) {
+        this.start = {
+            c: column,
+            r: row,
+        };
+
+        this.current = this.start;
+
+        this.grid = grid;
+
+        this.stack = [];
+
+        this.initialize();
     }
-    
-    this.current = this.start;
-    
-    this.grid = grid;
-    
-    this.stack = [];
-    
-    this.grid.getCell(
-      this.current.c, 
-      this.current.r
-      ).isVisited = true
-  }
-  
-  update() {
-    this.move(1, 0);
-  }
-  
-  move(moveColumn, moveRow) {
-    let target = {
-      c: this.current.c + moveColumn, 
-      r: this.current.r + moveRow
-    };
-    
-    if(this.grid.isCellIndexOutOfBounds(target.c, target.r)) {
-      return;
+
+    initialize() {
+        let startCell = this.grid.getCell(this.current.c, this.current.r);
+        startCell.isVisited = true;
+        this.stack.push(startCell);
     }
-    
-    let targetCell = this.grid.getCell(target.c, target.r);
-    let currentCell = this.grid.getCell(this.current.c, this.current.r);
-    
-    if(moveColumn === -1) {
-      currentCell.walls.left = false;
-    } else if(moveColumn === 1) {
-      targetCell.walls.left = false;
-    } else if(moveRow === -1) {
-      currentCell.walls.top = false
-    } else if(moveRow === 1) {
-      targetCell.walls.top = false;
+
+    update() {
+        this.move(1, 0);
     }
-    
-    targetCell.isVisited = true;
-    
-    this.current = target;
-  }
+
+    move(moveColumn, moveRow) {
+        let target = {
+            c: this.current.c + moveColumn,
+            r: this.current.r + moveRow,
+        };
+
+        if (this.grid.isCellIndexOutOfBounds(target.c, target.r)) {
+            return;
+        }
+
+        let targetCell = this.grid.getCell(target.c, target.r);
+        let currentCell = this.grid.getCell(this.current.c, this.current.r);
+
+        if (moveColumn === -1) {
+            currentCell.walls.left = false;
+        } else if (moveColumn === 1) {
+            targetCell.walls.left = false;
+        } else if (moveRow === -1) {
+            currentCell.walls.top = false;
+        } else if (moveRow === 1) {
+            targetCell.walls.top = false;
+        }
+
+        targetCell.isVisited = true;
+
+        this.current = target;
+    }
 }
