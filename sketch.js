f84ba62e7137f6d14256d591523eaf79a55acb7f
Wen Xiang <tohwenxiang@gmail.com>
2021-05-15 20:31:03 +0800

    Add a slider for controlling framerate

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 let mazeGenerationDepthFirstSearch = function (P5) {
     let maze;
     let mazeRenderer;
+    let frameRateSlider;
+    let canvas;
     P5.setup = function () {
         maze = new Maze(P5, 20, 20, 15);
         mazeRenderer = new MazeRenderer(P5, maze);
 
-        P5.frameRate(5);
-        P5.createCanvas(maze.width, maze.height);
+        canvas = P5.createCanvas(maze.width, maze.height).parent(
+            'canvas-container'
+        );
+
+        frameRateSlider = P5.createSlider(1, 60, 10, 1).parent(
+            'settings-container'
+        );
+        P5.frameRate(frameRateSlider.value());
     };
 
     P5.draw = function () {
+        P5.frameRate(frameRateSlider.value());
         P5.background(0);
         maze.update();
         mazeRenderer.draw();
